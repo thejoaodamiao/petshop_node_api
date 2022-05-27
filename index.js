0aaa7b9f65a41e76e37e0b3c2902757121f3e7e9
thejoadamiao <thejoaodamiao@gmail.com>
2022-05-27 15:19:08 -0300

    add procfile

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cep = require("./api/cep");
 
 //express
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
