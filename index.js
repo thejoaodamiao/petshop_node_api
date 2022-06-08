b67e968319121561cb45605668c085946050aa93
thejoadamiao <thejoaodamiao@gmail.com>
2022-06-08 15:45:09 -0300

    changes

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get("/api/saudacao", (req, res) => {
     saudacao = { saudacao: "bom dia" };
   } else if (hour < 17) {
     saudacao = { saudacao: "boa tarde" };
-  } else if (hour > 17 && hour < 6) {
+  } else {
     saudacao = { saudacao: "boa noite" };
   }
   res.status(200).json(saudacao);
